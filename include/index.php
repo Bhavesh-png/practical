2e08d65ea15998d8d2d80a29c83acc2610d0909e
bhavesh <bhavesh.mahendra.borse@gmail.com>
2026-02-22 01:47:35 +0530

    commit 13: include in php code

---
+++ include/index.php
@@ -0,0 +1,21 @@
+<?php
+//copies the content of the file (php/html/css/js/text) and include it in your php file.
+//Section of our website became resable changes only need to be made in one place.
+    include ("Footer.html");
+    ?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home page</title>
+</head>
+<body>
+
+<?php include "Header.html"; ?>
+
+<h2>Welcome to our Home Page</h2>
+<p>This is home page content.</p>
+
+<?php include "Footer.html"; ?>
+
+</body>
+</html>
